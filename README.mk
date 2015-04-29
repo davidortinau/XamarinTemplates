2dc8683475fff7b687858d75cc732c81dd1b988a
David Ortinau <dave@rendr.io>
2015-04-28 19:37:24 -0500

    added a readme

---
+++ README.mk
@@ -0,0 +1,5 @@
+To add a template in Xamarin Studio, open Preferences > Code Templates.
+
+Select Add, paste the template code into the Template Text panel, give it a shortcut and fill out any other fields you like.
+
+In a code file, start typing the shortcut and hit tab to insert the template.
